494ae33084d5be45278f5f6259a0e3d2fa38b4d8
mysterymartial <bolasax16@gmail.com>
2024-07-30 10:11:06 +0100

    phasegate

---
+++ Agbaosi/Part2/task1.py
@@ -0,0 +1,9 @@
+# create a iterator from 1000 to 3000
+# create a condition that check if the number is an even number
+# print the result
+
+
+
+for number in range(1000,3000):
+	if number % 2 == 0:
+		print(f"{number.}  end = ' , '")
